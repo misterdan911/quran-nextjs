16cc8c8d175025bbc3888b647c605940537d6517
Danu <dciptadi@gmail.com>
2024-05-25 03:08:13 +0700

    add home page

---
+++ app/homepage/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@mui/material';
+import styles from './Homepage.module.css';
+
+const HomePage = () => {
+  return (
+    <>
+      <div className={`${styles.ArabQuranIslamic140} text-9xl h-screen flex items-center justify-center`}>
+        121
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
---
+++ css.d.ts
@@ -0,0 +1,6 @@
+// css.d.ts
+declare module '*.module.css' {
+    const classes: { [key: string]: string };
+    export default classes;
+  }
+  
